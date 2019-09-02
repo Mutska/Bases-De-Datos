183e6e61d2b50e4b46802850a8222e599269c3be
mutska <mutska@ciencias.unam.mx>
2019-09-01 22:42:20 -0500

    Archivos Iniciales

---
+++ src/empleado.py
@@ -0,0 +1,11 @@
+class Empleado:
+    def __init__(self, nombre, fecha_de_nacimiento, direccion, sexo, grado, horario, licencia, historial):
+        self.nombre = nombre
+        self.fecha_de_nacimiento = fecha_de_nacimiento
+        self.direccion = direccion
+        self.sexo = sexo
+        self.grado = grado
+        self.horario = horario
+        self.licencia = licencia
+        self.historial = historial
+
