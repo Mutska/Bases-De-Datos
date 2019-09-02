0f049974d9ff2565735750f4ef51b709a15161b5
jmct1208 <mcolin@ciencias.unam.mx>
2019-09-02 01:53:09 -0500

    Agregué dos clases de las entidades licencia y exámen médico.

---
+++ src/examen_medico.py
@@ -0,0 +1,11 @@
+class ExamenMedico:
+    def __init__(self, id_examen, talla, peso, presion, estatus, fecha, hora, cedula_medico, id_empleado):
+        self.id_examen = id_examen
+        self.talla = talla
+        self.peso = peso
+        self.presion = presion
+        self.estatus = estatus
+        self.fecha = fecha
+        self.hora = hora
+        self.cedula_medico = cedula_medico
+        self.id_empleado = id_empleado
---
+++ src/licencia.py
@@ -0,0 +1,6 @@
+class Licencia:
+    def __init__(self, id_licencia, fecha_inicio, fecha_vencimiento, id_empleado):
+        self.id_licencia = id_licencia
+        self.fecha_inicio = fecha_inicio
+        self.fecha_vencimiento = fecha_vencimiento
+        self.id_empleado = id_empleado        
