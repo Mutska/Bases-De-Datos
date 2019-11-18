f7f564eeec9c7adbc55150d3928eafe2d2d87f14
mutska <mutska@ciencias.unam.mx>
2019-11-17 19:50:42 -0600

    borrado de K

--- tarea6/Consultas/Consultas_marco.sql
+++ tarea6/Consultas/Consultas_marco.sql
@@ -2,10 +2,6 @@
 
 --Ejercicio J  
 select info.razonSocial, count(info.num_empleados) as num_empleados, YEAR(info.fechaIngreso) anio,  DATEPART(qq,info.fechaIngreso) trimestre , info.genero  from (select e.razonSocial, t.RFC,  count(m.CURP) num_empleados, genero, fechaIngreso from Empresa e join Trabajar t  on e.RFC = t.RFC join Empleado m on m.CURP = t.CURP group by e.razonSocial,t.RFC , m.genero, t.fechaIngreso) as info group by info.razonSocial, info.fechaIngreso, info.genero
-
---Ejercicio K aun no sale :v
-select e.razonSocial, m.CURP, max(t.salarioQuincenal)  from Empresa e join Trabajar t  on e.RFC = t.RFC join Empleado m on m.CURP = t.CURP group by e.razonSocial, m.CURP, t.salarioQuincenal
- 
 --Ejercicio N
 select p.numProy, p.nombre, p.fechaFin, p.fechaInicio, p.RFC from Colaborar c join Dirigir D on c.CURP = d.CURP join Proyecto p  on p.numProy = c.numProy 
 
