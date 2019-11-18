fb605432ca32c9c37d14717e3987d61f1384aad0
mutska <mutska@ciencias.unam.mx>
2019-11-17 18:12:51 -0600

    avance

---
+++ tarea6/Consultas/Consultas_marco.sql
@@ -0,0 +1,13 @@
+
+
+--Ejercicio J  
+select info.razonSocial, count(info.num_empleados) as num_empleados, YEAR(info.fechaIngreso) anio,  DATEPART(qq,info.fechaIngreso) trimestre , info.genero  from (select e.razonSocial, t.RFC,  count(m.CURP) num_empleados, genero, fechaIngreso from Empresa e join Trabajar t  on e.RFC = t.RFC join Empleado m on m.CURP = t.CURP group by e.razonSocial,t.RFC , m.genero, t.fechaIngreso) as info group by info.razonSocial, info.fechaIngreso, info.genero
+
+--Ejercicio K aun no sale :v
+select e.razonSocial, m.CURP, max(t.salarioQuincenal)  from Empresa e join Trabajar t  on e.RFC = t.RFC join Empleado m on m.CURP = t.CURP group by e.razonSocial, m.CURP, t.salarioQuincenal
+ 
+--Ejercicio N
+
+
+
+
