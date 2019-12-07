ce180271788fd955b1e1432b7a7f27bf12d425fd
mutska <mutska@ciencias.unam.mx>
2019-12-07 14:27:24 -0600

    Procedures actualizado

--- Proyecto/SQL Scripts/Procedures.sql
+++ Proyecto/SQL Scripts/Procedures.sql
@@ -4,3 +4,9 @@
 --Se les asigna un Bono de 1500 pesos por cada 5 anios que esten trabajando
 CREATE PROCEDURE SetBonos AS DECLARE @fecha DATETIME SET @fecha = CONVERT(VARCHAR,GETDATE(),103) INSERT INTO Bono(monto , fechaEntrega, idPersona) SELECT (c.num * 1500), @fecha, c.idPersona FROM  (SELECT idPersona, floor((DATEDIFF(year, fechaContratacion, GETDATE())) / 5) num FROM Persona WHERE DATEDIFF(year, fechaContratacion, GETDATE()) > 4 AND (DATEDIFF(year, fechaNacimiento, GETDATE())) % 5  = 0 ) AS c;
 
+--Procedimiento almacenado que nos permite cambiar el precio de las salsa por presentacion 
+CREATE PROCEDURE UpdatePrice @presentacion nvarchar(255), @precio money AS UPDATE Salsa SET precio = @precio WHERE presentacion = @presentacion;
+--Ejemplo
+--select  *  from Salsa where presentacion = 'cuarto';
+--exec UpdatePrice @presentacion = 'Cuarto', @precio = 35;
+--select  *  from Salsa where presentacion = 'cuarto';
